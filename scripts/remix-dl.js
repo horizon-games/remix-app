67e5a08cfd43cf8df2fafe9f40b0b11c7b865883
Peter Kieltyka <peter.kieltyka@pressly.com>
2018-02-22 16:10:16 -0500

    Update remix-dl.js to fetch the latest remix app published on github

--- scripts/remix-dl.js
+++ scripts/remix-dl.js
@@ -6,49 +6,94 @@ const fs = require('fs')
 const path = require('path')
 const unzipper = require('unzipper')
 const rimraf = require('rimraf')
-const pkgJson = require('../package.json')
 
-const destPath = path.join(os.tmpdir(), 'remix.zip')
+const fetchLatestRemixDownloadURL = (cb) => {
+  const repoListURL = 'https://api.github.com/repos/ethereum/browser-solidity/contents/?ref=gh-pages'
+  const githubReq = {
+    url: repoListURL,
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.88 Safari/537.36'
+    }
+  }
 
-const download = (url, dest, ret, err) => {
-  var file = fs.createWriteStream(dest)
-  var sendReq = request.get(url)
+  request.get(githubReq, (err, resp, body) => {
+    if (err) {
+      console.log('github error:', err)
+      process.exit(1)
+    }
 
-  sendReq.on('response', function(response) {
-      if (response.statusCode !== 200) {
-        return err('Unable to download remix file, status: ' + response.statusCode)
+    if (resp.statusCode !== 200) {
+      console.log('github: failed to fetch remix download url, got status', resp.statusCode)
+      process.exit(1)
+    }
+
+    const repo = JSON.parse(body)
+    let downloadURL
+    
+    repo.forEach((obj) => {
+      if (obj.name.match(/remix-[a-z0-9]+\.zip/i)) {
+        downloadURL = obj.download_url
       }
-  });
+    })
+    
+    if (downloadURL) {
+      cb(downloadURL)
+    } else {
+      console.log('github: failed to get latest remix download url')
+      process.exit(1)
+    }
+  })
+}
 
-  sendReq.on('error', function (err) {
-      fs.unlink(dest)
-      return err(err.message)
+const fetchRemixApp = (url, destPath) => {
+  const ret = () => {
+    const appDir = path.join(path.dirname(fs.realpathSync(__filename)), '../build/app')
+  
+    rimraf(appDir, () => {
+      const reader = fs.createReadStream(destPath)
+      reader.pipe(unzipper.Extract({ path: appDir }))
+      fs.unlinkSync(destPath)
+    }) 
+  }
+  
+  const err = (msg) => {
+    console.error(msg)
+  }
+
+  const file = fs.createWriteStream(destPath)
+  const sendReq = request.get(url)
+
+  sendReq.on('response', (response) => {
+    if (response.statusCode !== 200) {
+      return err('Unable to download remix file, status: ' + response.statusCode)
+    }
+  })
+
+  sendReq.on('error', (err) => {
+    fs.unlink(destPath)
+    return err(err.message)
   })
 
   sendReq.pipe(file)
 
-  file.on('finish', function() {
-      file.close(ret)
+  file.on('finish', () => {
+    file.close(ret)
   })
 
-  file.on('error', function(err) {
-      fs.unlink(dest)
-      return err(err.message)
+  file.on('error', (err) => {
+    fs.unlink(destPath)
+    return err(err.message)
   })
 }
 
-const ret = () => {
-  const appDir = path.join(path.dirname(fs.realpathSync(__filename)), '../build/app')
 
-  rimraf(appDir, () => {
-    const reader = fs.createReadStream(destPath)
-    reader.pipe(unzipper.Extract({ path: appDir }))
+
+fetchLatestRemixDownloadURL((downloadURL) => {
+  console.log(`latest remix download url at ${downloadURL}, downloading & unpacking..`)
+
+  const destPath = path.join(os.tmpdir(), 'remix.zip')
+  if (fs.existsSync(destPath)) {
     fs.unlinkSync(destPath)
-  }) 
-}
-
-const err = (msg) => {
-  console.error(msg)
-}
-
-download(pkgJson.remixFile, destPath, ret, err)
+  }
+  fetchRemixApp(downloadURL, destPath)
+})
